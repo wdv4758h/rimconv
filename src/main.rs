a1053c7f072fddc60e7680a68309769eef6e3b80
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2016-08-03 20:51:08 +0800

    feat: basic image convertion support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+#[macro_use]
+extern crate clap;
+extern crate image;
+
+use std::fs::File;
+
+use clap::App;
+use image::ImageFormat;
+
 fn main() {
-    println!("Hello, world!");
+
+    ////////////////////
+    // Parse Arguments
+    ////////////////////
+
+    let yml = load_yaml!("arguments.yml");
+    let arguments = App::from_yaml(yml).get_matches();
+
+    let input = arguments.value_of("input").unwrap();
+    let output = arguments.value_of("output").unwrap();
+    let format = arguments.value_of("format").unwrap();
+
+    ////////////////////
+    // Extra Handling
+    ////////////////////
+
+    let format = match format {
+        "png" => ImageFormat::PNG,
+        "jpeg" => ImageFormat::JPEG,
+        "gif" => ImageFormat::GIF,
+        // "webp" => ImageFormat::WEBP, // no encoder now
+        "ppm" => ImageFormat::PPM,
+        // "tiff" => ImageFormat::TIFF, // no encoder now
+        // "tga" => ImageFormat::TGA,   // no encoder now
+        // "bmp" => ImageFormat::BMP,   // no encoder now
+        "ico" => ImageFormat::ICO,
+        // "hdr" => ImageFormat::HDR,   // waiting for new release
+        _ => unreachable!(),
+    };
+
+    ////////////////////
+    // Image Processing
+    ////////////////////
+
+    let img = image::open(input).unwrap();
+    img.save(&mut File::create(output).unwrap(),
+             format).unwrap();
 }
